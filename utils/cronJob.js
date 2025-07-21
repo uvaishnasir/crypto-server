329f0c02ca2dfbc7a470f06aa1fbb085b1e6fbd8
Mohd Uvaish <81020580+uvaishnasir@users.noreply.github.com>
2025-07-21 10:35:38 +0530

    Update cronJob.js

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -7,6 +7,7 @@ const COINGECKO_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1";
 
 const runCronJob = () => {
+  console.log("Cron job loaded, runs every hour...");
   cron.schedule("0 * * * *", async () => {
     try {
       console.log("⏰ Cron Job: Fetching data from CoinGecko...");
